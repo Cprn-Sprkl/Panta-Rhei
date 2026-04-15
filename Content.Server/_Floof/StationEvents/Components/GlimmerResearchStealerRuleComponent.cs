ac65e68b6449cf5eb1003943da30f0ad66e80e49
Cprn-Sprkl <sprkl7992@gmail.com>
2026-04-15 08:56:08 -0400

    Stealer Event
    Adds the Glimmer Research Stealer event.

---
+++ Content.Server/_Floof/StationEvents/Components/GlimmerResearchStealerRuleComponent.cs
@@ -0,0 +1,16 @@
+using Content.Server.StationEvents.Events;
+using Content.Server._Floof.StationEvents.Events;
+using Robust.Shared.Audio;
+
+namespace Content.Server._Floof.StationEvents.Components;
+
+[RegisterComponent, Access(typeof(GlimmerResearchStealerRule))]
+public sealed partial class GlimmerResearchStealerRuleComponent : Component
+{
+    [DataField]
+    public int MinToSteal = 1;
+    [DataField]
+    public int MaxToSteal = 8;
+    [DataField]
+    public SoundSpecifier GlimmerStealSound = new SoundPathSpecifier("/Audio/_DV/CosmicCult/ability_siphon.ogg");
+}
