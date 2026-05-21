86bebaff6f701ed1885a39ddfa5356803e1fe679
Cprn-Sprkl <sprkl7992@gmail.com>
2026-05-21 03:51:07 -0400

    Reference fixes

---
+++ Content.Server/_Goobstation/BlockHandsOnBuckle/Components/BlockHandsOnBuckleComponent.cs
@@ -0,0 +1,7 @@
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+namespace Content.Server._Goobstation.BlockHandsOnBuckle.Components;
+
+[RegisterComponent]
+public sealed partial class BlockHandsOnBuckleComponent : Component;
