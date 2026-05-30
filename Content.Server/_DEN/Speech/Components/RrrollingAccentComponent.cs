c7e1c34cc13a7dce46b0305055886dc499746139
Cprn-Sprkl <sprkl7992@gmail.com>
2026-05-30 08:58:08 -0400

    Rrrolling Accent
    A port of the Tajaran accent from the Den (added there by chuga-git) as a non-species specific accent trait.

---
+++ Content.Server/_DEN/Speech/Components/RrrollingAccentComponent.cs
@@ -0,0 +1,4 @@
+namespace Content.Server._DEN.Speech.Components;
+
+[RegisterComponent]
+public sealed partial class RrrollingAccentComponent : Component { } // Euphoria | Changed from TajaranAccentComponent as it will not be base for Tajaran here.
