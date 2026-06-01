8251d5d153755004ce73a6447efdb7047995bce8
Cprn-Sprkl <sprkl7992@gmail.com>
2026-06-01 06:00:21 -0400

    Rewrite
    Does things differently

--- Content.Server/_DEN/Speech/Components/RrrollingAccentComponent.cs
+++
@@ -1,4 +0,0 @@
-namespace Content.Server._DEN.Speech.Components;
-
-[RegisterComponent]
-public sealed partial class RrrollingAccentComponent : Component { } // Euphoria | Changed from TajaranAccentComponent as it will not be base for Tajaran here.
